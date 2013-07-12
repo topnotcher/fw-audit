418e47ee273ef73c63c884bfa2b7f7482de4671f
Greg Bowser <topnotcher@gmail.com>
2013-07-12 15:54:04 -0400

    Start a date machine for dumping the configuration

--- fwsm.rb
+++ fwsm.rb
@@ -11,6 +11,54 @@ SSH_HOST=ARGV[0]
 #outbuf = ''
 #last_cmd = ''
 
+
+class FwsmDumper
+
+	@@show_context = 'show context'
+	@@changeto_system = 'changeto system'
+
+	@@changeto_context = 'changeto context'
+
+	@@show_run = 'show run'
+
+	def initialize(fwsm)
+		@fwsm = fwsm
+		@state = 'init'
+
+		@contexts = nil
+	end
+
+
+	def ready(prompt)
+		if @state == 'init'
+			@state = 'derp' 
+			@fwsm.cmd(@@changeto_system)
+			@fwsm.cmd(@@show_context)
+		end 
+
+	end
+
+	def cmd_result(cmd, data)
+		if cmd == @@show_context and not @contexts
+			populate_contexts(data)
+		end
+
+	end
+
+
+	def populate_contexts(data)
+		@contexts = []
+		
+		data.each_line do |line|
+			next unless line.start_with? '*',' '
+			@contexts << line[1..line.index(' ',1)-1]
+		end 
+
+		puts @contexts
+	end			
+	
+end
+
 class Fwsm
 
 	@@prompt = /> $/
@@ -20,7 +68,7 @@ class Fwsm
 
 	def initialize(host,user,pass)
 		@pass = pass
-		@cmds = ['terminal pager 0', 'changeto system', 'show context', 'changeto context OIS-Webservers','show run']
+		@cmds = ['terminal pager 0']
 		@ssh = Net::SSH.start(host ,user, {:password => pass, :auth_methods => ['password']})
 		@state = 'new'
 		@ignore_echo_chars = 0
@@ -34,6 +82,10 @@ class Fwsm
 				ch.on_data {|chn,data| handle_data(chn,data)}
 			end
 		end  
+	end
+
+	def cmd(cmd) 
+		@cmds << cmd 
 	end
 
 	def run
@@ -54,18 +106,31 @@ class Fwsm
 			end
 		
 		elsif data =~ @@enprompt
-			@state = 'enabled'
-			puts @buf if @last_cmd
+			if @state != 'enabled'
+				@state = 'enabled'
+				@dumper = FwsmDumper.new(self)
+			end
+
+			if @last_cmd
+				@dumper.cmd_result(@last_cmd.strip, @buf)
+			else
+				@dumper.ready(data)
+			end
+	
 			unless @cmds.size == 0
 				@buf = ''
+
+				# echoed commands always echo back with CRLF even if I send LF
 				@last_cmd = @cmds.shift + @@crlf
 				@ignore_echo_chars = @last_cmd.length 
+
 				chn.send_data(@last_cmd)
 			else 
 				10.times { chn.send_data("exit\n") unless (!chn.active? || chn.closing?)}
 			end	
 
 		elsif @state == 'enabled'
+			# filter echoed commands 
 			if data.length == 1 and @ignore_echo_chars > 0
 				@ignore_echo_chars -= 1
 			else 
