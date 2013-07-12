84d1ad639570207f750cb41eec21cb171deb2d70
Greg Bowser <topnotcher@gmail.com>
2013-07-12 14:42:54 -0400

    show context, change to, dump config

--- fwsm.rb
+++ fwsm.rb
@@ -7,52 +7,61 @@ SSH_USER=ARGV[1]
 SSH_PASS=ARGV[2]
 SSH_HOST=ARGV[0]
 
-config_file = File.open('fwsm.conf','w')
-outbuf = ''
-last_cmd = ''
+#config_file = File.open('fwsm.conf','w')
+#outbuf = ''
+#last_cmd = ''
 
-ssh = Net::SSH.start(SSH_HOST ,SSH_USER, {:password => SSH_PASS, :auth_methods => ['password']})
+class Fwsm
 
-pass_cnt = 0
+	@@prompt = /> $/
+	@@enprompt = /# $/
+	@@pwprompt = /^Password:/	
 
-cmds = ['terminal pager 0', 'show run']
+	def initialize(host,user,pass)
+		@pass = pass
+		@cmds = ['terminal pager 0', 'changeto system', 'show context', 'changeto context OIS-Webservers','show run']
+		@ssh = Net::SSH.start(host ,user, {:password => pass, :auth_methods => ['password']})
+		@state = 'new'
+		@pwtries = 0
 
-ssh.open_channel do |chan|
-	chan.send_channel_request('shell') do |ch, success|
-		abort 'Failed to open shell channel!' unless success
-
-		ch.on_data do |chn, data|
-
-			outbuf += data
-
-			outbuf = '' if outbuf == last_cmd or outbuf =~ /^[\r\n]+$/
-	
-			# normal prompt - send enable 
-			chn.send_data("en\n") if data =~ /> $/
-
-			# should pick up any password prompt (particularly aftersending enable)
-			if data =~ /^Password:/
-				abort 'Too many password attempts!' unless (pass_cnt += 1) <= 3
-				chn.send_data(SSH_PASS + "\n")
+		@ssh.open_channel do |chan|
+			chan.send_channel_request('shell') do |ch,success|
+				raise 'Failed to open shell channel!' unless success
+				ch.on_data {|chn,data| handle_data(chn,data)}
 			end
+		end  
+	end
 
-			# enable prompt: do show etcetc
-			if data =~ /# $/
+	def run
+		@ssh.loop
+	end
 
-				config_file << outbuf if last_cmd != ''
+	def handle_data(chn, data)
 
-				unless cmds.size == 0
-					outbuf = ''
-					last_cmd = cmds.shift
-					chn.send_data(last_cmd + "\n") 
-				else
-					10.times do
-						chn.send_data("exit\n") unless (!chn.active? || chn.closing?)
-					end
-				end	
-			end # enable prompt 
+		puts data
+
+		if @state == 'new' and data =~ @@prompt
+			@state = 'normal'
+			chn.send_data("enable\n")
+
+		elsif @state == 'normal' and data =~ @@pwprompt
+			if (@pwtries += 1) < 4
+				chn.send_data(@pass + "\n")
+			else
+				raise 'Too many pw tries!'
+			end
+		
+		elsif data =~ @@enprompt
+			unless @cmds.size == 0
+				chn.send_data(@cmds.shift + "\n")
+			else 
+				10.times { chn.send_data("exit\n") unless (!chn.active? || chn.closing?)}
+			end			
 		end
 	end
 end
-ssh.loop
-config_file.close
+
+
+fwsm = Fwsm.new(SSH_HOST,SSH_USER,SSH_PASS)
+fwsm.run
+
