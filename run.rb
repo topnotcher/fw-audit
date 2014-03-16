b8e7d5709d386d8e3e67935d2e1bf6f86b923530
Greg Bowser <topnotcher@gmail.com>
2014-03-16 12:47:37 -0400

    Moves the SSH and git configurations into the config.yml

--- run.rb
+++ run.rb
@@ -3,17 +3,21 @@ require_relative 'syslog'
 require_relative 'commiter'
 require 'yaml'
 
-abort "usage: ruby fwsm.rb host user pass dir" if ARGV.size < 4
+abort "usage: ruby fwsm.rb config" if ARGV.size < 1
 
-SSH_USER=ARGV[1]
-SSH_PASS=ARGV[2]
-SSH_HOST=ARGV[0]
-REPO_DIR=ARGV[3]
+CONFIG=ARGV[0]
 
-config = YAML::load(File.open('config.yml'))
+puts "config: '%s'" % [CONFIG]
+config = YAML::load(File.open(CONFIG))
 
-manager = FWSMConfigManager.new(SSH_HOST, SSH_USER, SSH_PASS, config[:user_map], REPO_DIR)
-syslog = FWSMChangePublisher.new(config[:address],config[:port], config[:context_map])
+manager = FWSMConfigManager.new(
+	config[:fwsm][:host],
+	config[:fwsm][:user],
+	config[:fwsm][:pass],
+	config[:user_map],
+	config[:repo]
+)
+syslog = FWSMChangePublisher.new(config[:syslog][:address],config[:syslog][:port], config[:context_map])
 syslog.subscribe(FWSMChangeAggregator.new(manager))
 
 
